ef2e7e8fa1c633d8e2b74266cea7093fc2eae494
github username <github email address>
2020-08-07 23:03:34 -0400

    fisrt commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const users = http:"//localhost:3000/users"
+
+function submitData(name, email){
+
+    let options = {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name:name, email:email
+        })
+    };
+    
+    fetch(users, options)
+    .then(resp => resp.json())
+    .then(console.log)
+
+
+}// f submitData
+
+
